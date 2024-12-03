82241ef7fa721105529c0c547358e331bccd93ca
SamuelChiasson30 <samuelchiasson30@gmail.com>
2024-12-03 12:26:07 -0500

    Binding des toggle buttons avec l'objet greenhouse

---
+++ Automate/Models/GreenHouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automate.Models
+{
+    public static class GreenHouse
+    {
+        public static bool isLightOpen { get; set; } = false;
+        public static bool isFanActivated { get; set; } = false;
+        public static bool isHeaterActivated { get; set; } = false;
+        public static bool isWindowOpen { get; set; } = false;
+        public static bool isSprinklerActivated { get; set; } = false;
+    }
+}
--- Automate/ViewModels/GreenHouseViewModel.cs
+++ Automate/ViewModels/GreenHouseViewModel.cs
@@ -7,17 +7,12 @@ using System.Windows.Input;
 using System.Windows;
 using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace Automate.ViewModels
 {
     public class GreenHouseViewModel : INotifyPropertyChanged
     {
-        private bool _isWindowOpen;
-        private bool _isHeaterActivated;
-        private bool _isSprinklerActivated;
-        private bool _isLightOpen;
-        private bool _isFanActivated;
-
         private Window _window;
         public ICommand AddTaskCommand { get; }
         public ICommand UpdateTaskCommand { get; }
@@ -25,22 +20,61 @@ namespace Automate.ViewModels
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public GreenHouseViewModel(Window openedWindow)
+        public GreenHouseViewModel(Window openedWindow) => _window = openedWindow;
+
+        public bool IsWindowOpen
         {
-            _window = openedWindow;
+            get => GreenHouse.isWindowOpen;
+            set
+            {
+                GreenHouse.isWindowOpen = value;
+                NotifyOnPropertyChanged(nameof(IsWindowOpen));
+            }
         }
 
-        public bool IsWindowOpen { get => _isWindowOpen; set => _isWindowOpen = value; }
-        public bool IsHeaterActivated { get => _isHeaterActivated; set => _isHeaterActivated = value; }
-        public bool IsSprinklerActivated { get => _isSprinklerActivated; set => _isSprinklerActivated = value; }
-        public bool IsFanActivated { get => _isFanActivated; set => _isFanActivated = value; }
-        public bool IsLightOpen { get => _isLightOpen; set => _isLightOpen = value; }
+        public bool IsHeaterActivated
+        {
+            get => GreenHouse.isHeaterActivated;
+            set
+            {
+                GreenHouse.isHeaterActivated = value;
+                NotifyOnPropertyChanged(nameof(IsHeaterActivated));
+            }
+        }
+        public bool IsSprinklerActivated 
+        {
+            get => GreenHouse.isSprinklerActivated;
+            set
+            {
+                GreenHouse.isSprinklerActivated = value;
+                NotifyOnPropertyChanged(nameof(IsSprinklerActivated));
+            }
+        }
 
+        public bool IsFanActivated 
+        {
+            get => GreenHouse.isFanActivated;
+            set 
+            {
+                GreenHouse.isFanActivated = value;
+                NotifyOnPropertyChanged(nameof(IsFanActivated));
+            }
+
+        }
+
+        public bool IsLightOpen 
+        { 
+            get => GreenHouse.isLightOpen;
+            set
+            {
+                GreenHouse.isLightOpen = value;
+                NotifyOnPropertyChanged(nameof(IsLightOpen));
+            }
+        }
 
         protected void NotifyOnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
     }
 }
